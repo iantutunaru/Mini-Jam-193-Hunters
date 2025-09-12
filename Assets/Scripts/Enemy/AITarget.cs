f409821a63843533aa50e74952dd9ec3b8abe714
Ianis Tutunaru <129986472+iantutunaru@users.noreply.github.com>
2025-09-12 15:54:51 +0300

    Add Enemy with navigation and chase mechanic
    * Added AI Navigation package to the project.
    * Created a simple Enemy prefab that can follow the player.
    * Created an AITarget script that allows the enemy to chase the player.
    * Created a NavMesh prefab to generate NavMeshes of maps.

---
+++ Assets/Scripts/Enemy/AITarget.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AITarget : MonoBehaviour
+{
+    public Transform target;
+    public float attackDistance;
+    
+    [SerializeField]
+    private NavMeshAgent mAgent;
+    [SerializeField]
+    private Animator mAnimator;
+    
+    private float _mDistance;
+    
+    private static readonly int Attack = Animator.StringToHash(("Attack"));
+    
+    private void FixedUpdate()
+    {
+        _mDistance = Vector3.Distance(mAgent.transform.position, target.position);
+
+        if (_mDistance < attackDistance)
+        {
+            mAgent.isStopped = true;
+            //mAnimator.SetBool(Attack, true);
+        }
+        else
+        {
+            mAgent.isStopped = false;
+            //mAnimator.SetBool(Attack, false);
+            mAgent.destination = target.position;
+        }
+    }
+
+    private void OnAnimatorMove()
+    {
+        /*
+        if (mAnimator.GetBool(Attack) == false)
+        {
+            mAgent.speed = (mAnimator.deltaPosition / Time.fixedDeltaTime).magnitude;
+        }*/
+    }
+}
